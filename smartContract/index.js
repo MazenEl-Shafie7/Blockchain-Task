6f1ee6901260287d07cadc6f387f548d37820ac8
mazen <mazen@ibm.com>
2019-04-20 23:39:51 +0200

    added smart contract index.js and package.json

---
+++ smartContract/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+//const MyContract = require('./lib/globalFinance.js');
+const   smartContract = require('./lib/smartContract.js');
+
+module.exports.contracts = [ smartContract ];
